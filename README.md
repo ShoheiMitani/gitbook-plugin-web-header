b955ea4b512bde2e6bb5e7d47e01c6a1e188f403
ShoheiMitani <mitani49@gmail.com>
2017-02-06 01:11:33 +0900

    Release module.

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-This is test package....
+# Gitbook Web Header Plugin
+
+This plugin creates header element to all page.
+
+You can use it for your book in the book.json:
+
+```
+{
+    "plugins": ["web-header"]
+}
+```
+
+You can set the header title using the plugins configuration in the book.json:
+
+```
+"pluginsConfig": {
+    "webheader": {
+        "headerTitle" : "XXXX Title"
+    }
+}
+```
--- index.js
+++ index.js
@@ -1,27 +1,11 @@
-var fs = require('fs');
-var path = require('path');
-
 module.exports = {
-    // Map of hooks
-    hooks: {
-        "finish" : function () {
-            // Get configuration.
-            var filePath = this.options.pluginsConfig.webheader.headerPath;
-            var header   = path.join(process.cwd(), filePath);
-            var height   = this.options.pluginsConfig.webheader.height || 40;
-
-            fs.readFile(header, 'utf8', function (err, html) {
-                if (err) {
-                    console.log('Header html file was not found')
-                } else {
-                    // Adjust header width.
-
-                    $('.book-body').css('top', height + 'px');
-                    $('.book-summary').css('top',height + 'px');
-                    // Add customize header html.
-                    $('.book-summary').before($(html));
-                }
-            });
-        }
-    },
+    book: {
+        assets: "./assets",
+        js: [
+            "plugin.js"
+        ],
+        css: [
+            "web-header.css"
+        ]
+    }
 };
