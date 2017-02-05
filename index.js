51b4c80fb5048b731c4148e5bb1cd9723deec4d0
ShoheiMitani <mitani49@gmail.com>
2017-02-05 23:04:56 +0900

    Update index.js file

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
+var fs = require('fs');
+var path = require('path');
+
 module.exports = {
-    book: {
-        assets: "./assets",
-        js: [],
-        css: []
-    },
+    // Map of hooks
     hooks: {
-        "page:before": function(page) {
-            page.content = "# Title\n" +page.content;
-            return page;
+        "finish" : function () {
+            // Get configuration.
+            var filePath = this.options.pluginsConfig.webheader.headerPath;
+            var header   = path.join(process.cwd(), filePath);
+            var height   = this.options.pluginsConfig.webheader.height || 40;
+
+            fs.readFile(header, 'utf8', function (err, html) {
+                if (err) {
+                    console.log('Header html file was not found')
+                } else {
+                    // Adjust header width.
+
+                    $('.book-body').css('top', height + 'px');
+                    $('.book-summary').css('top',height + 'px');
+                    // Add customize header html.
+                    $('.book-summary').before($(html));
+                }
+            });
         }
-    }
+    },
 };
